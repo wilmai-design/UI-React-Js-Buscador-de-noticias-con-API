cdabd87d49d38678a7081e48e87244d1545c536b
Luis Perez Mosteiro <lperez@NTB-189.local>
2020-02-03 22:54:52 -0300

    Creando y programando custom Hooks para nuestras categorias

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    const OPCIONES = [
+        { value: 'general', label: 'General' },
+        { value: 'business', label: 'Negocios' },
+        { value: 'entertainment', label: 'Entretinimiento' },
+        { value: 'health', label: 'Salud' },
+        { value: 'science', label: 'Ciencia' },
+        { value: 'sport', label: 'Deportes' },
+        { value: 'technology', label: 'Tecnología' },
+    ]
+
+    // utilizar custom hook
+    const [ categoria, SelecNoticias ] = useSelect('general', OPCIONES);
+
     return (
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra noticias por Categoría</h2>
+
+                    <SelecNoticias />
 
                     <div className="input-field col s12">
                         <input 
---
+++ src/hooks/useSelect.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const useSelect = (stateInicial, opciones) => {
+    
+    // state del custom hook
+    const [state, actualizarState] = useState(stateInicial);
+    
+    const SelecNoticias = () => (
+        <select
+            className="browser-default"
+            value={state}
+            onChange={e => actualizarState(e.target.value)}
+        >
+            {opciones.map(opcion => (
+                <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+            ))}
+        </select>
+        );
+
+        return [state, SelecNoticias];
+}
+
+export default useSelect;
