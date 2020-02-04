ac91422918d9120de2877a9ffd4c7fd77af4c7ae
Luis Perez Mosteiro <lperez@ntb-158.d01.keplertech.com>
2020-02-04 17:54:21 -0300

    Vinculando API junto con el formulario y las categorias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `
+      https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=c9aa1a7c0e054c16a94c6fc8fe571866`
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      console.log(noticias);
+      
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header 
@@ -10,7 +28,9 @@ function App() {
       />
 
       <div className="container white">
-        <Formulario />   
+        <Formulario 
+          guardarCategoria={guardarCategoria}
+        />   
       </div>
     </Fragment>
   );
