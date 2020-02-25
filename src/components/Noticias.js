627fd286004617ae3f497be9250e0bd1292181ea
Wilmai <artegraff.stile@gmail.com>
2020-02-25 16:19:18 -0300

    Añadiendo Proptypes para documentar nuestros componentes
    Añadiendo Proptypes para documentar nuestros componentes y señalar que tipo de datos se requieren para una correcta utilización de nuestra UI.

--- src/components/Noticias.js
+++ src/components/Noticias.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
  const Noticia = ({noticia}) => {
 
     // extraer los datos
     const { urlToImage, url, title, description, source } = noticia;
+
+    const imagen = {urlToImage} ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title}/>
+            <span className="card-title">{source.name}</span>
+        </div>
+    : null;
+
     return ( 
         <div className="col s12 m6 l4">
              <div className="card">
-                 <div className="card-image">
-                    <img src={urlToImage} alt={title}/>
-                    <span className="card-title">{source.name}</span>
-                 </div>
-                 
-
+                 {imagen}
                  <div className="card-content">
                     <h3>{title}</h3>
                     <p>{description}</p>
@@ -33,5 +37,9 @@ import React from 'react';
         </div>
     );
  }
+
+ Noticia.propTypes = {
+    noticia: PropTypes.object.isRequired
+}
 
  export default Noticia;
